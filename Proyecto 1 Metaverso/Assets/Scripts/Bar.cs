d2eaa4ad4988f8c8d033a12b21cd5b2a5dc48a0d
AlejandroMorabito <amorabito@correo.unimet.edu.ve>
2024-10-14 23:58:35 -0400

    movimiento
    Movimiento de barra y de pelota

--- Proyecto 1 Metaverso/Assets/Scripts/Bar.cs
+++ Proyecto 1 Metaverso/Assets/Scripts/Bar.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class Bar : MonoBehaviour
 {
+public Rigidbody2D RigidBody2D;
+private float inputValue;
+public float moveSpeed = 25;
+private Vector2 direction;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,21 @@ public class Bar : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        inputValue = Input.GetAxisRaw("Horizontal");
+        if (inputValue == 1)
+        {
+            direction = Vector2.right;
+        }
+        else if (inputValue == -1)
+        {
+            direction = Vector2.left;
+        }
+        else
+        {
+            direction = Vector2.zero;
+        }
+
+        RigidBody2D.AddForce(direction * moveSpeed * Time.deltaTime * 100);
+
     }
 }
