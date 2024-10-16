ebee47610699e4582dce018a87a5db89a8065756
daecheverria <decheverria@correo.unimet.edu.ve>
2024-10-16 00:31:00 -0400

    Evitar bucle

--- Proyecto 1 Metaverso/Assets/Scripts/Ball.cs
+++ Proyecto 1 Metaverso/Assets/Scripts/Ball.cs
@@ -20,7 +20,7 @@ public class Ball : MonoBehaviour
 
     IEnumerator Delay(float delay)
     {
-        yield return new WaitForSeconds(delay); 
+        yield return new WaitForSeconds(delay);
         velocity.x = Random.Range(-1f, 1f);
         velocity.y = 1;
         rigidBody2D.velocity = velocity.normalized * speed;
@@ -28,6 +28,17 @@ public class Ball : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        Vector2 newVelocity = rigidBody2D.velocity;
+
+        if (Mathf.Abs(newVelocity.y) < 0.5f)
+        {
+
+            newVelocity.y = newVelocity.y > 0 ? 1f : -1f;
+        }
+
+        newVelocity = newVelocity.normalized * speed;
+        rigidBody2D.velocity = newVelocity;
+
         rigidBody2D.velocity = rigidBody2D.velocity.normalized * speed;
         AudioBall.Play();
         if (collision.gameObject.CompareTag("DeadZone"))
@@ -39,7 +50,7 @@ public class Ball : MonoBehaviour
 
     public void ResetBall()
     {
-        transform.position = new Vector2(0,-3.5f);
+        transform.position = new Vector2(0, -3.5f);
         rigidBody2D.velocity = Vector2.zero;
         StartCoroutine(Delay(0.5f));
     }
